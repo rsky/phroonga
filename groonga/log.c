112cd7a4551b4d26526291dfd5d0fc07b1f85209
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2013-02-11 01:15:19 +0900

    Year++

--- groonga/log.c
+++ groonga/log.c
@@ -3,7 +3,7 @@
  *
  * @package     phroonga
  * @author      Ryusuke SEKIYAMA <rsky0711@gmail.com>
- * @copyright   2012 Ryusuke SEKIYAMA
+ * @copyright   2012-2013 Ryusuke SEKIYAMA
  * @license     http://www.opensource.org/licenses/mit-license.php  MIT License
  */
 
